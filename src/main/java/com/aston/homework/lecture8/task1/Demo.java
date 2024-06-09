9fc117777d3fe7cf6a5ce513b916ac410950255d
kluchnikov-dv <kluchnikov.dv@gmail.com>
2024-06-09 19:17:57 +0300

    changed 2 lines (Hello world to Hello big world) and (temp to other temp)

--- src/main/java/com/aston/homework/lecture8/task1/Demo.java
+++ src/main/java/com/aston/homework/lecture8/task1/Demo.java
@@ -3,8 +3,8 @@ package com.aston.homework.lecture8.task1;
 public class Demo {
 
     public static void main(String[] args) {
-        System.out.println("Hello world.");
-        Temp temp = new Temp("temp");
+        System.out.println("Hello big world.");
+        Temp temp = new Temp("other temp");
         System.out.println(temp.getName());
     }    
 }
