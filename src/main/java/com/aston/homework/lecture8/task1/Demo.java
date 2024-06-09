0c9d4dab7934fc814f4c192f7ffdf13365d6731f
kluchnikov-dv <kluchnikov.dv@gmail.com>
2024-06-09 17:56:55 +0300

    add Temp class and modificated Demo class in dev branch

--- src/main/java/com/aston/homework/lecture8/task1/Demo.java
+++ src/main/java/com/aston/homework/lecture8/task1/Demo.java
@@ -3,6 +3,8 @@ package com.aston.homework.lecture8.task1;
 public class Demo {
 
     public static void main(String[] args) {
-        System.out.println("Hello world.");   
+        System.out.println("Hello world.");
+        Temp temp = new Temp("temp");
+        System.out.println(temp.getName());
     }    
 }
---
+++ src/main/java/com/aston/homework/lecture8/task1/Temp.java
@@ -0,0 +1,19 @@
+package com.aston.homework.lecture8.task1;
+
+public class Temp {
+
+    private String name;
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    Temp(String name) {
+        this.name = name;
+    }
+    
+}
